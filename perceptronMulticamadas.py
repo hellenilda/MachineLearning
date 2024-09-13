25e01398b7e61c2614b1a41d8306d654858eb78e
hellenilda <hellen95limaaraujo@gmail.com>
2024-09-13 10:54:29 -0300

    feat: gerar pesos aleatórios

--- perceptronMulticamadas.py
+++ perceptronMulticamadas.py
@@ -16,10 +16,21 @@ entradas = np.array([[0,0],
 
 saidas = np.array([[0],[1],[1],[0]])
 
-pesos0 = np.array([[-0.424, -0.740, -0.961],
-                  [0.358, -0.577, -0.469]])
+# Inicializa os pesos entre a camada de entrada e a camada oculta
+  # np.random.random((2,3)) cria uma matriz de valores aleatórios entre 0 e 1
+    # com 2 neurônios na camada de entrada e 3 neurônios na camada oculta.
+  # Multiplicar por 2 e subtrair 1 transforma esses valores para o intervalo [-1, 1],
+    # permitindo que os pesos tenham tanto valores positivos quanto negativos.
+pesos0 = 2 * np.random.random((2,3)) - 1
 
-pesos1 = np.array([[-0.017],[-0.893],[0.148]])
+# Inicializa os pesos entre a camada oculta e a camada de saída.
+  # np.random.random((3,1)) cria uma matriz de valores aleatórios entre 0 e 1
+    # com 3 neurônios na camada oculta e 1 neurônio na camada de saída.
+  # Multiplicar por 2 e subtrair 1 transforma esses valores para o intervalo [-1, 1],
+    # garantindo que os pesos possam assumir valores tanto positivos quanto negativos,
+    # o que pode ajudar a rede a convergir mais rapidamente e a explorar melhor o espaço de solução.
+pesos0 = 2 * np.random.random((3,1)) - 1
+
 
 epocas = 10000 # Quantidade de vezes p/ atualizar pesos (erro = 0)
 taxaAprendizagem = 0.3
